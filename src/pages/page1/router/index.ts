3220c02edf7f3f7b4f7984bd203168d734342022
liuyinghao <1194827696@qq.com>
2020-09-28 09:59:53 +0800

    feat: 提交修改路由

--- src/pages/page1/router/index.ts
+++ src/pages/page1/router/index.ts
@@ -3,11 +3,12 @@ import Router from "vue-router"
 import routes from "./children"
 
 Vue.use(Router)
+// 已经分成多页面了 我感觉就不要子模块了
 let routesList: Array<any> = [
   {
     path: "/",
     component: () => import("@/pages/page1/home.vue"),
-    children: [...routes],
+    // children: [...routes],
   },
 ]
 routesList = routesList.concat(routes)
